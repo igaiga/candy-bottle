89ce2a94fc6f414c2242460bdd29f70a465168d8
Kuniaki IGARASHI <igaiga@gmail.com>
2014-01-04 15:56:16 +0900

    Add Tweet#bring.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet
-  def self.bring
+  attr_reader :stream, :client
+  def initialize
     config = Pit.get("candy-bottle", require: {
                        twitter: {
                          consumer_key: 'consumer_key',
@@ -8,13 +9,32 @@ class Tweet
                          access_token_secret: 'access_token_secret' }
                      })
 
-    client = Twitter::Streaming::Client.new do |twitter_config|
-      twitter_config.consumer_key= config[:twitter][:consumer_key]
-      twitter_config.consumer_secret= config[:twitter][:consumer_secret]
-      twitter_config.access_token= config[:twitter][:access_token]
-      twitter_config.access_token_secret= config[:twitter][:access_token_secret]
+    @client = Twitter::Client.new do |twitter_config|
+      twitter_config.consumer_key = config[:twitter][:consumer_key]
+      twitter_config.consumer_secret = config[:twitter][:consumer_secret]
+      twitter_config.access_token = config[:twitter][:access_token]
+      twitter_config.access_token_secret = config[:twitter][:access_token_secret]
     end
-    client.sample do |t|
+
+    # streaming api
+    @stream = Twitter::Streaming::Client.new do |twitter_config|
+      twitter_config.consumer_key = config[:twitter][:consumer_key]
+      twitter_config.consumer_secret = config[:twitter][:consumer_secret]
+      twitter_config.access_token = config[:twitter][:access_token]
+      twitter_config.access_token_secret = config[:twitter][:access_token_secret]
+    end
+  end
+
+  def bring
+    @stream.user do |obj|
+      case obj
+      when Twitter::Tweet
+        p "#{ obj.id } : #{obj.text},#{obj.user.name}, #{obj.user.id}"
+      when Twitter::DirectMessage
+        #puts "It's a direct message!"
+      when Twitter::Streaming::StallWarning
+        #warn "Falling behind!"
+      end
     end
   end
 end
