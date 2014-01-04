b41fdbfcd12d1246b1a843977d283059edfc71c4
Kuniaki IGARASHI <igaiga@gmail.com>
2014-01-04 16:54:44 +0900

    Add targets in seed.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# -*- coding: utf-8 -*-
+target_words = ['いがいが', 'いがさん']
+target_words.each do |word|
+  Target.new(word: word).save! unless Target.where(word: word).exists?
+end
