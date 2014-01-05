8d85ad363d87cd1f1671eab353199a2db0906153
Kuniaki IGARASHI <igaiga@gmail.com>
2014-01-05 11:13:10 +0900

    Modify seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-target_words = ['いがいが', 'いがさん']
+target_words = ['いがいが', 'いがさん', 'igaiga']
 target_words.each do |word|
   Target.new(word: word).save! unless Target.where(word: word).exists?
 end
